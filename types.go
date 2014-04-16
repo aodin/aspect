d337199644a9938a330af115d6720570222f623d
aodin <aaronoellis@gmail.com>
2014-04-16 14:58:43 -0600

    Added Inet and Double types

--- types.go
+++ types.go
@@ -91,3 +91,51 @@ type Serial struct{}
 func (s Serial) Create(d Dialect) (string, error) {
 	return "SERIAL NOT NULL", nil
 }
+
+type Double struct {
+	NotNull    bool
+	Unique     bool
+	PrimaryKey bool
+}
+
+func (s Double) Create(d Dialect) (string, error) {
+	compiled := "DOUBLE PRECISION"
+	attrs := make([]string, 0)
+	if s.PrimaryKey {
+		attrs = append(attrs, "PRIMARY KEY")
+	}
+	if s.NotNull {
+		attrs = append(attrs, "NOT NULL")
+	}
+	if s.Unique {
+		attrs = append(attrs, "UNIQUE")
+	}
+	if len(attrs) > 0 {
+		compiled += fmt.Sprintf(" %s", strings.Join(attrs, " "))
+	}
+	return compiled, nil
+}
+
+type Inet struct {
+	NotNull    bool
+	Unique     bool
+	PrimaryKey bool
+}
+
+func (s Inet) Create(d Dialect) (string, error) {
+	compiled := "INET"
+	attrs := make([]string, 0)
+	if s.PrimaryKey {
+		attrs = append(attrs, "PRIMARY KEY")
+	}
+	if s.NotNull {
+		attrs = append(attrs, "NOT NULL")
+	}
+	if s.Unique {
+		attrs = append(attrs, "UNIQUE")
+	}
+	if len(attrs) > 0 {
+		compiled += fmt.Sprintf(" %s", strings.Join(attrs, " "))
+	}
+	return compiled, nil
+}
