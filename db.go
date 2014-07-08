7dc8ea272661a27154f2da2dfcc7dcdb5b6b24e0
aodin <aaronoellis@gmail.com>
2014-07-08 11:02:00 -0600

    Added a transaction wrapper with query and execute methods

--- db.go
+++ db.go
@@ -12,10 +12,15 @@ type Executable interface {
 	Compiles
 }
 
-// TODO dialect
+// DB includes the current sql.DB connection and the associated Dialect
 type DB struct {
 	conn    *sql.DB
 	dialect Dialect
+}
+
+func (db *DB) Begin() (*TX, error) {
+	tx, err := db.conn.Begin()
+	return &TX{tx, db}, err
 }
 
 func (db *DB) Close() error {
@@ -51,7 +56,8 @@ func (db *DB) Query(stmt Executable, args ...interface{}) (*Result, error) {
 	return &Result{rows: rows, stmt: s}, nil
 }
 
-// Query the statement and populate the interface with all results
+// QueryAll will query the statement and populate the interface with all
+// results
 func (db *DB) QueryAll(s Executable, i interface{}) error {
 	result, err := db.Query(s)
 	if err != nil {
@@ -60,8 +66,7 @@ func (db *DB) QueryAll(s Executable, i interface{}) error {
 	return result.All(i)
 }
 
-// Query the statement and populate the interface with one result
-// TODO Return an error if there is more than one result?
+// QueryOne will query the statement and populate the interface with one result
 func (db *DB) QueryOne(s Executable, i interface{}) error {
 	result, err := db.Query(s)
 	// Close the result rows or sqlite3 will open another connection
@@ -107,4 +112,77 @@ func Connect(driver, credentials string) (*DB, error) {
 		return nil, err
 	}
 	return &DB{conn: db, dialect: dialect}, nil
+}
+
+type TX struct {
+	*sql.Tx
+	db *DB
+}
+
+func (tx *TX) Query(stmt Executable, args ...interface{}) (*Result, error) {
+	// Initialize a list of empty parameters
+	params := Params()
+
+	// TODO Columns are needed for name return types, tag matching, etc...
+	s, err := stmt.Compile(tx.db.dialect, params)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO When to use the given arguments?
+	// TODO If args are structs, maps, or slices, unpack them
+	// Use any arguments given to Query() over compiled arguments
+	if len(args) == 0 {
+		args = params.args
+	}
+
+	rows, err := tx.Tx.Query(s, args...)
+	if err != nil {
+		return nil, err
+	}
+	// Wrap the sql rows in a result
+	return &Result{rows: rows, stmt: s}, nil
+}
+
+// QueryAll will query the statement and populate the interface with all
+// results
+func (tx *TX) QueryAll(s Executable, i interface{}) error {
+	result, err := tx.Query(s)
+	if err != nil {
+		return err
+	}
+	return result.All(i)
+}
+
+// QueryOne will query the statement and populate the interface with one result
+func (tx *TX) QueryOne(s Executable, i interface{}) error {
+	result, err := tx.Query(s)
+	// Close the result rows or sqlite3 will open another connection
+	defer result.rows.Close()
+	if err != nil {
+		return err
+	}
+	return result.One(i)
+}
+
+// Execute the statement
+// Execute should be used when no results are expected
+// TODO What should the return type be?
+func (tx *TX) Execute(stmt Executable, args ...interface{}) (sql.Result, error) {
+	// Initialize a list of empty parameters
+	params := Params()
+
+	// TODO Columns are needed for name return types, tag matching, etc...
+	s, err := stmt.Compile(tx.db.dialect, params)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO When to use the given arguments?
+	// TODO If args are structs, maps, or slices, unpack them
+	// Use any arguments given to Query() over compiled arguments
+	if len(args) == 0 {
+		args = params.args
+	}
+	return tx.Exec(s, args...)
 }
