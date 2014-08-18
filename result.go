1454bde44b977020b4fa32deddf77e5dce959289
aodin <aaronoellis@gmail.com>
2014-08-18 12:30:48 -0600

    Added ability to query results into a map[string]interface{}

--- result.go
+++ result.go
@@ -3,11 +3,15 @@ package aspect
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"reflect"
 )
 
 var (
 	ErrNoResult = errors.New("aspect: no result to return")
+	ErrWrongMap = errors.New(
+		"aspect: maps must have key type string and value type interface{}",
+	)
 )
 
 type Result struct {
@@ -44,6 +48,30 @@ func (r *Result) One(i interface{}) error {
 			dest[i] = elem.Field(i).Addr().Interface()
 		}
 		return r.rows.Scan(dest...)
+	} else if elem.Kind() == reflect.Map {
+		// TODO This operation should not be repeated
+		names, _ := r.rows.Columns()
+
+		result := make([]interface{}, len(names))
+		addresses := make([]interface{}, len(names))
+		for index := range addresses {
+			addresses[index] = &result[index]
+		}
+		if err := r.rows.Scan(addresses...); err != nil {
+			return fmt.Errorf("aspect: error while scanning map: %s", err)
+		}
+
+		m, ok := elem.Interface().(map[string]interface{})
+		if !ok {
+			return ErrWrongMap
+		}
+		// Make a new mapping item
+		// TODO Confirm that it is not a nil map
+		// TODO what if there are redundant names?
+		for index, name := range names {
+			m[name] = result[index]
+		}
+		return nil
 	} else {
 		return r.rows.Scan(elem.Addr().Interface())
 	}
@@ -76,6 +104,34 @@ func (r *Result) All(i interface{}) error {
 			}
 			sliceValue.Set(reflect.Append(sliceValue, newElem))
 		}
+	} else if elem.Kind() == reflect.Map {
+		names, _ := r.rows.Columns()
+		result := make([]interface{}, len(names))
+		addresses := make([]interface{}, len(names))
+		for index := range addresses {
+			addresses[index] = &result[index]
+		}
+		for r.rows.Next() {
+			// Create a new slice element
+			newElem := reflect.MakeMap(elem)
+
+			if err := r.rows.Scan(addresses...); err != nil {
+				return fmt.Errorf("aspect: error while scanning map: %s", err)
+			}
+
+			m, ok := newElem.Interface().(map[string]interface{})
+			if !ok {
+				return ErrWrongMap
+			}
+			// Make a new mapping item
+			// TODO Confirm that it is not a nil map
+			// TODO what if there are redundant names?
+			for index, name := range names {
+				m[name] = result[index]
+			}
+			sliceValue.Set(reflect.Append(sliceValue, newElem))
+		}
+		return nil
 	} else {
 		// Create a single interface for receiving
 		for r.rows.Next() {
