4849f9592b09dbb7120aeef8520a9a1a2c99f4b9
Aaron O. Ellis <aaronoellis@gmail.com>
2014-11-23 22:18:32 -0700

    Added Must methods to the connection interface

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package aspect
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 // Connection is a common interface for database connections or transactions
@@ -12,6 +13,12 @@ type Connection interface {
 	QueryAll(stmt Executable, i interface{}) error
 	QueryOne(stmt Executable, i interface{}) error
 	String(stmt Executable) string // Parameter-less output for logging
+
+	// Must operations will panic on error
+	MustExecute(stmt Executable, args ...interface{}) sql.Result
+	MustQuery(stmt Executable, args ...interface{}) *Result
+	MustQueryAll(stmt Executable, i interface{})
+	MustQueryOne(stmt Executable, i interface{}) bool
 }
 
 // Both DB and TX should implement the Connection interface
@@ -60,22 +67,25 @@ func (db *DB) Dialect() Dialect {
 	return db.dialect
 }
 
+func (db *DB) compile(stmt Executable) (string, *Parameters, error) {
+	// Initialize a list of empty parameters
+	params := Params()
+
+	// Compile with the database connection's current dialect
+	s, err := stmt.Compile(db.dialect, params)
+	return s, params, err
+}
+
 // Execute executes the Executable statement with optional arguments. It
 // returns the database/sql package's Result object, which may contain
 // information on rows affected and last ID inserted depending on the driver.
 func (db *DB) Execute(stmt Executable, args ...interface{}) (sql.Result, error) {
-	// Initialize a list of empty parameters
-	params := Params()
-
-	// TODO Columns are needed for name return types, tag matching, etc...
-	s, err := stmt.Compile(db.dialect, params)
+	s, params, err := db.compile(stmt)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO When to use the given arguments?
-	// TODO If args are structs, maps, or slices, unpack them
-	// Use any arguments given to Query() over compiled arguments
+	// Use any arguments given to Execute() over compiled arguments
 	if len(args) == 0 {
 		args = params.args
 	}
@@ -85,17 +95,11 @@ func (db *DB) Execute(stmt Executable, args ...interface{}) (sql.Result, error) 
 // Query executes an Executable statement with the optional arguments. It
 // returns a Result object, that can scan rows in various data types.
 func (db *DB) Query(stmt Executable, args ...interface{}) (*Result, error) {
-	// Initialize a list of empty parameters
-	params := Params()
-
-	// TODO Columns are needed for name return types, tag matching, etc...
-	s, err := stmt.Compile(db.dialect, params)
+	s, params, err := db.compile(stmt)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO When to use the given arguments?
-	// TODO If args are structs, maps, or slices, unpack them
 	// Use any arguments given to Query() over compiled arguments
 	if len(args) == 0 {
 		args = params.args
@@ -132,10 +136,101 @@ func (db *DB) QueryOne(stmt Executable, i interface{}) error {
 }
 
 // String returns parameter-less SQL. If an error occurred during compilation,
-// then an empty string will be returned.
+// then the string output of the error will be returned.
 func (db *DB) String(stmt Executable) string {
-	compiled, _ := stmt.Compile(db.dialect, Params())
+	compiled, err := stmt.Compile(db.dialect, Params())
+	if err != nil {
+		return err.Error()
+	}
 	return compiled
+}
+
+// MustExecute will panic on error.
+func (db *DB) MustExecute(stmt Executable, args ...interface{}) sql.Result {
+	s, params, err := db.compile(stmt)
+	if err != nil {
+		panic(fmt.Sprintf(
+			"aspect: failed to compile (%s): %s",
+			stmt,
+			err,
+		))
+	}
+	if len(args) == 0 {
+		args = params.args
+	}
+	result, err := db.conn.Exec(s, args...)
+	if err != nil {
+		panic(fmt.Sprintf(
+			"aspect: failed to exec (%s) with parameters (%v): %s",
+			s,
+			args,
+			err,
+		))
+	}
+	return result
+}
+
+// MustQuery
+func (db *DB) MustQuery(stmt Executable, args ...interface{}) *Result {
+	s, params, err := db.compile(stmt)
+	if err != nil {
+		panic(fmt.Sprintf(
+			"aspect: failed to compile (%s): %s",
+			stmt,
+			err,
+		))
+	}
+
+	// Use any arguments given to Query() over compiled arguments
+	if len(args) == 0 {
+		args = params.args
+	}
+
+	rows, err := db.conn.Query(s, args...)
+	if err != nil {
+		panic(fmt.Sprintf(
+			"aspect: failed to query (%s) with parameters (%v): %s",
+			s,
+			args,
+			err,
+		))
+	}
+	// Wrap the sql rows in a result
+	return &Result{rows: rows, stmt: s}
+}
+
+// MustQueryAll
+func (db *DB) MustQueryAll(stmt Executable, i interface{}) {
+	result := db.MustQuery(stmt)
+	if err := result.All(i); err != nil {
+		// TODO get parameters from result?
+		panic(fmt.Sprintf(
+			"aspect: failed to query all (%s): %s",
+			result.stmt,
+			err,
+		))
+	}
+}
+
+// MustQueryOne
+func (db *DB) MustQueryOne(stmt Executable, i interface{}) bool {
+	result := db.MustQuery(stmt)
+
+	// Close the result rows or sqlite3 will open another connection
+	defer result.rows.Close()
+
+	err := result.One(i)
+	if err == ErrNoResult {
+		return false
+	} else if err != nil {
+		// TODO get parameters from result?
+		panic(fmt.Sprintf(
+			"aspect: failed to query one (%s): %s",
+			result.stmt,
+			err,
+		))
+	}
+	return true
 }
 
 // Connect connects to the database using the given driver and credentials.
@@ -161,6 +256,15 @@ type TX struct {
 	dialect Dialect
 }
 
+func (tx *TX) compile(stmt Executable) (string, *Parameters, error) {
+	// Initialize a list of empty parameters
+	params := Params()
+
+	// Compile with the database connection's current dialect
+	s, err := stmt.Compile(tx.dialect, params)
+	return s, params, err
+}
+
 // Begin returns the existing transaction. TODO Are nested transactions
 // possible? And on what dialects?
 func (tx *TX) Begin() (Transaction, error) {
@@ -172,21 +276,32 @@ func (tx *TX) Commit() error {
 	return tx.Tx.Commit()
 }
 
-// Query executes an Executable statement with the optional arguments
-// using the current transaction. It returns a Result object, that can scan
-// rows in various data types.
-func (tx *TX) Query(stmt Executable, args ...interface{}) (*Result, error) {
-	// Initialize a list of empty parameters
-	params := Params()
-
-	// TODO Columns are needed for name return types, tag matching, etc...
-	s, err := stmt.Compile(tx.dialect, params)
+// Execute executes the Executable statement with optional arguments using
+// the current transaction. It returns the database/sql package's Result
+// object, which may contain information on rows affected and last ID inserted
+// depending on the driver.
+func (tx *TX) Execute(stmt Executable, args ...interface{}) (sql.Result, error) {
+	s, params, err := tx.compile(stmt)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO When to use the given arguments?
-	// TODO If args are structs, maps, or slices, unpack them
+	// Use any arguments given to Query() over compiled arguments
+	if len(args) == 0 {
+		args = params.args
+	}
+	return tx.Exec(s, args...)
+}
+
+// Query executes an Executable statement with the optional arguments
+// using the current transaction. It returns a Result object, that can scan
+// rows in various data types.
+func (tx *TX) Query(stmt Executable, args ...interface{}) (*Result, error) {
+	s, params, err := tx.compile(stmt)
+	if err != nil {
+		return nil, err
+	}
+
 	// Use any arguments given to Query() over compiled arguments
 	if len(args) == 0 {
 		args = params.args
@@ -222,27 +337,92 @@ func (tx *TX) QueryOne(stmt Executable, i interface{}) error {
 	return result.One(i)
 }
 
-// Execute executes the Executable statement with optional arguments using
-// the current transaction. It returns the database/sql package's Result
-// object, which may contain information on rows affected and last ID inserted
-// depending on the driver.
-func (tx *TX) Execute(stmt Executable, args ...interface{}) (sql.Result, error) {
-	// Initialize a list of empty parameters
-	params := Params()
-
-	// TODO Columns are needed for name return types, tag matching, etc...
-	s, err := stmt.Compile(tx.dialect, params)
+// MustExecute will panic on error.
+func (tx *TX) MustExecute(stmt Executable, args ...interface{}) sql.Result {
+	s, params, err := tx.compile(stmt)
 	if err != nil {
-		return nil, err
+		panic(fmt.Sprintf(
+			"aspect: failed to compile (%s): %s",
+			stmt,
+			err,
+		))
+	}
+	if len(args) == 0 {
+		args = params.args
+	}
+	result, err := tx.Exec(s, args...)
+	if err != nil {
+		panic(fmt.Sprintf(
+			"aspect: failed to exec (%s) with parameters (%v): %s",
+			s,
+			args,
+			err,
+		))
+	}
+	return result
+}
+
+// MustQuery
+func (tx *TX) MustQuery(stmt Executable, args ...interface{}) *Result {
+	s, params, err := tx.compile(stmt)
+	if err != nil {
+		panic(fmt.Sprintf(
+			"aspect: failed to compile (%s): %s",
+			stmt,
+			err,
+		))
 	}
 
-	// TODO When to use the given arguments?
-	// TODO If args are structs, maps, or slices, unpack them
 	// Use any arguments given to Query() over compiled arguments
 	if len(args) == 0 {
 		args = params.args
 	}
-	return tx.Exec(s, args...)
+
+	rows, err := tx.Tx.Query(s, args...)
+	if err != nil {
+		panic(fmt.Sprintf(
+			"aspect: failed to query (%s) with parameters (%v): %s",
+			s,
+			args,
+			err,
+		))
+	}
+	// Wrap the sql rows in a result
+	return &Result{rows: rows, stmt: s}
+}
+
+// MustQueryAll
+func (tx *TX) MustQueryAll(stmt Executable, i interface{}) {
+	result := tx.MustQuery(stmt)
+	if err := result.All(i); err != nil {
+		// TODO get parameters from result?
+		panic(fmt.Sprintf(
+			"aspect: failed to query all (%s): %s",
+			result.stmt,
+			err,
+		))
+	}
+}
+
+// MustQueryOne
+func (tx *TX) MustQueryOne(stmt Executable, i interface{}) bool {
+	result := tx.MustQuery(stmt)
+
+	// Close the result rows or sqlite3 will open another connection
+	defer result.rows.Close()
+
+	err := result.One(i)
+	if err == ErrNoResult {
+		return false
+	} else if err != nil {
+		// TODO get parameters from result?
+		panic(fmt.Sprintf(
+			"aspect: failed to query one (%s): %s",
+			result.stmt,
+			err,
+		))
+	}
+	return true
 }
 
 // Rollback calls the wrapped transactions Rollback method.
@@ -251,9 +431,12 @@ func (tx *TX) Rollback() error {
 }
 
 // String returns parameter-less SQL. If an error occurred during compilation,
-// then an empty string will be returned.
+// then the string output of the error will be returned.
 func (tx *TX) String(stmt Executable) string {
-	compiled, _ := stmt.Compile(tx.dialect, Params())
+	compiled, err := stmt.Compile(tx.dialect, Params())
+	if err != nil {
+		return err.Error()
+	}
 	return compiled
 }
 
@@ -289,6 +472,22 @@ func (tx *fakeTX) QueryAll(stmt Executable, i interface{}) error {
 
 func (tx *fakeTX) QueryOne(stmt Executable, i interface{}) error {
 	return tx.tx.QueryOne(stmt, i)
+}
+
+func (tx *fakeTX) MustExecute(stmt Executable, args ...interface{}) sql.Result {
+	return tx.tx.MustExecute(stmt, args...)
+}
+
+func (tx *fakeTX) MustQuery(stmt Executable, args ...interface{}) *Result {
+	return tx.tx.MustQuery(stmt, args...)
+}
+
+func (tx *fakeTX) MustQueryAll(stmt Executable, i interface{}) {
+	tx.tx.MustQueryAll(stmt, i)
+}
+
+func (tx *fakeTX) MustQueryOne(stmt Executable, i interface{}) bool {
+	return tx.tx.MustQueryOne(stmt, i)
 }
 
 func (tx *fakeTX) Rollback() error {
